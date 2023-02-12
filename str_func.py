6f9d32aae1cd8e85e6ed4b2f5204c966aaba73e5
GAU <gau@1jz.ru>
2023-02-12 20:26:37 +0300

    Edit file str_func.py and add def py_str_tit and edd docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def py_str_up(word:str):
+    """
+    Функцию принимает на вход строку и возвращает ее со всеми заглавными буквами
+    :param word: Строка
+    :return: Строка
+    """
     return word.upper()
+
+
+def py_str_tit(word:str):
+    """
+    Функция делает заглавными первые буквы каждого слова в строке
+    :param word: Строка или слово
+    :return: Строка
+    """
+    return word.title().strip()
